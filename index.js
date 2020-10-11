9741e3f9eeb4a993cc81292cc1a3385b0d2fa682
SantoshCode <santsubcode@gmail.com>
2020-10-11 21:55:09 +0545

    📦 NEW: Welcome Message

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 #!/usr/bin/env node
 
-const clearConsole = require('clear-any-console')
+const pkgJSON = require('./package.json')
+const welcome = require('cli-welcome')
 
-// clear the console
-clearConsole()
+welcome({
+	title: pkgJSON.name,
+	tagLine: `Howdy, nice to meet ya!`,
+	description: pkgJSON.description,
+	version: pkgJSON.version,
+	bgColor: `#FADC00`,
+	color: `#000000`,
+	bold: true,
+	clear: true,
+})
 
 console.log(`
 Hi there, This is Santosh Subedi - AKA SantoshCode
