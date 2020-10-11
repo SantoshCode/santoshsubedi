ee6fea8c10c3f98ea936a8ab437a5edf8a3253d3
SantoshCode <santsubcode@gmail.com>
2020-10-11 21:58:45 +0545

    Tiyo

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ welcome({
 
 console.log(`
 Hi there, This is Santosh Subedi - AKA SantoshCode
-- I'm a sparklesFull Stack Engineer[beta]sparkles
+- I'm a Stack Engineer[beta]sparkles
 - I’m currently working as a Full Stack Developer!
 - I’m currently learning AI for production
 - Fun fact: I love to watch thriller movies/series!
