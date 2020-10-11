ced50270d06b35fcb56ef54ab12b568d61279d41
SantoshCode <santsubcode@gmail.com>
2020-10-11 20:27:19 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+console.log(`
+Hi there, This is Santosh Subedi - AKA SantoshCode
+- I'm a sparklesFull Stack Engineer[beta]sparkles
+- I’m currently working as a Full Stack Developer!
+- I’m currently learning AI for production
+- Fun fact: I love to watch thriller movies/series!
+`)
